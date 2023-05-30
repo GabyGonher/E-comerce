c801e0b7b99ae36ddd024cae3f9cdc688641d656
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2023-05-30 12:41:35 -0600

    Diseño de tarjeta de producto, etiqueta de clasificacion, precio

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import Layout from '../../Components/Layout';
-
+import Card from '../../Components/Card';
 function Home() {
   return (
     <Layout>
       <h1>Home</h1>
+      <Card />
     </Layout>
   );
 }
