7f1c139cc89730d061f0da732e60091a1d65dad4
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2023-06-06 14:32:52 -0600

    Consumo de API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
 import Layout from '../../Components/Layout';
 import Card from '../../Components/Card';
+
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+    // console.log(data);
+  }, []);
+
   return (
     <Layout>
       <h1>Home</h1>
-      <Card />
+      {items?.map(() => {
+        return <Card />;
+      })}
+      {/* <Card /> */}
     </Layout>
   );
 }
