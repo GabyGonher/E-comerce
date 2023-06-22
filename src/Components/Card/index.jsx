d64bd342a1e0f03dbad556d2c263b679747ae686
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2023-06-21 20:05:23 -0600

    contador de productos en carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react';
+import { AppContext } from '../../Context'
 const Card = (data) => {
+
+  const context  = useContext(AppContext)
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
@@ -11,8 +15,9 @@ const Card = (data) => {
           src={data.data.images[0]}
           alt={data.data.title}
         />
-        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
-          +
+        <div 
+        className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1" onClick={() => context.setCount(context.count + 1)} >
+        +
         </div>
       </figure>
       <p className="flex justify-between">
