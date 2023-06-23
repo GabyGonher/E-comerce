18e1471b588f0f82855ea9ca6a9fd8ed1aafcbc8
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2023-06-22 21:48:46 -0600

    apartado de producto de detalle, funcionalidad de abrir al darle click

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { AppContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+  const context  = useContext(AppContext)
 
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+    <aside 
+    className={`${context.productOpen ? 'flex' : 'hidden'} product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
 <div className='flex justify-between items-center p-6'>
   <h2 className='font-medium text-xl'> Detail</h2>
   <div>
