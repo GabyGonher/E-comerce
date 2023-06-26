d0cfbf78d76615611ea42bd52a01eda6b8e83c50
GabyGonher <114185794+GabyGonher@users.noreply.github.com>
2023-06-26 15:22:18 -0600

    Modal de my order que muestra productos del carrito

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -7,6 +7,7 @@ import MyOrders from '../MyOrders';
 import NotFound from '../NotFound';
 import SignIn from '../SignIn';
 import Navbar from '../../Components/Navbar';
+import CheckoutSideMenu  from '../../Components/CheckoutSideMenu';
 
 const AppRoutes = () => {
   let routes = useRoutes([
@@ -26,6 +27,7 @@ function App() {
     <BrowserRouter>
       <AppRoutes />
       <Navbar />
+      <CheckoutSideMenu />
     </BrowserRouter>
     </AppContextProvider>
   );
